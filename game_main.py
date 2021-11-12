bab0a6bf671be2fbed225b4ef31979c676883e48
timad <diudin.tv@phystech.edu>
2021-11-12 18:13:42 +0300

    Появление экрана pygame-a

--- game_main.py
+++ game_main.py
@@ -2,7 +2,21 @@ import pygame
 import pygame.draw as dr
 import game_visualisation as vis
 
+
 class Game:
     def __init__(self, money=100):
-        pass
+        pygame.init()
+        screen = pygame.display.set_mode((1000, 600))
+        clock = pygame.time.Clock()
 
+        finished = False
+        while not finished:
+            for our_event in pygame.event.get():
+                if our_event.type == pygame.QUIT:
+                    finished = True
+            pygame.display.update()
+            clock.tick(30)
+        pygame.quit()
+
+
+gggg = Game()
