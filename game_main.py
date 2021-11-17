c05fc87a1f85735042bc4e20e1bf258527535e76
timad <diudin.tv@phystech.edu>
2021-11-17 19:50:36 +0300

    Добавил башенные основания и изменил функции для рисования задника

--- game_main.py
+++ game_main.py
@@ -9,13 +9,14 @@ class Game:
         pygame.init()
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         clock = pygame.time.Clock()
+        self.towers = [0] * 999
 
         finished = False
         while not finished:
             for our_event in pygame.event.get():
                 if our_event.type == pygame.QUIT:
                     finished = True
-            vis.draw_background(screen)
+            vis.draw_background(screen, 1, self.towers)
             pygame.display.update()
             clock.tick(30)
         pygame.quit()
--- game_visualisation.py
+++ game_visualisation.py
@@ -1,6 +1,17 @@
 import pygame
 
 
-def draw_background(screen):
-    background_img = pygame.image.load("Textures/Background.png").convert_alpha()
+def draw_background(screen, level, towers):
+    """Рисует задний фон и места башни/места под башни
+    screen - окно для рисования
+    level - номер уровня
+    towers - список с номерами, обозначающими виды башен
+    """
+    background_img = pygame.image.load("Textures/Background" + str(level) + ".png").convert_alpha()
     screen.blit(background_img, (0, 0))
+    tower_spot_img = pygame.image.load("Textures/TowerSpot.png").convert_alpha()
+    level_design = open("level_designs.txt", "r")
+    design = level_design.readlines()[level].split()
+    for i in range(int(design[1])):
+        x, y = int(design[2 * i + 2]), int(design[2 * i + 3])
+        screen.blit(tower_spot_img, (x, y))
