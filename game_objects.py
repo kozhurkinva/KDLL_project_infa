06027b43d58b48cc22fc926050ce958ccc801350
timad <diudin.tv@phystech.edu>
2021-11-26 00:10:12 +0300

    Добавил измерение оставшегося для врага пройденного расстояния (кроме функции move_opponent, её всё ещё надоменять, но я не особо понимаю, что в ней происходит), слегка добавил подписей к классам в game_objects, изменил соответственно способу определения расстояния врага до базы чекер башен на ближайшего врага

--- game_objects.py
+++ game_objects.py
@@ -4,7 +4,14 @@ import game_constants as g_c
 
 
 class Tower:
-    def __init__(self, x, y, cause):
+    def __init__(self, x, y, cause, enemy_list):
+        """
+        Инициализация класса Tower (все возможные башни).
+        У каждой есть своё положение на карте (x, y), причина срабатывания её действия (cause), стоимость её постройки
+        (cost), время, которое должно пройти до возможности срабатывания следующего действия (reload_time), расстояние,
+        на котором башня детектит происходящее (range) и каждой передаётся список существующих на данный мамент врагов.
+        Также башни имеют атрибут charged_time, хранящий в себе время простоя башни с последнего действия
+        """
         self.x = x
         self.y = y
         self.cause = cause
@@ -12,17 +19,42 @@ class Tower:
         self.cost = 0
         self.reload_time = 1
         self.charged_time = 0
+        self.range = 0
+        self.enemy_list = enemy_list
 
     def check_cause(self):
+        """
+        Проверяет выполнение личного условия башни, изменяет в связи с этим флаг ready_for_action.
+        Возможные условия:
+        always_ready - готова работать вне зависимости от внешних условий и нуждается лишь во времени на перезарядку
+        enemy_in_range - враг находится в радиусе поражения башни
+        *_enemy_in_range - особый подвид врага * (определяется через его type) находится в радиусе поражения башни
+
+        В случае выполнения личного условия срабатывания и прохождения достаточного для перезарядки времени, функция
+        вызывает срабатывание особого для башни действия (action).
+        """
         closest_enemy = None
         self.charged_time += 1  # reloading
         if self.cause == "always_ready":
             self.ready_for_action = True
         elif self.cause == "enemy_in_range":
-            closest_enemy = Warrior
-            pass  # FIXME
+            closest_enemy = None
+            range_for_closest = 999999999999999
+            for enemy in self.enemy_list:
+                if (self.x - enemy.x) ** 2 + (self.y - enemy.y) ** 2 <= self.range ** 2:
+                    self.ready_for_action = True
+                    if enemy.distance <= range_for_closest:
+                        closest_enemy = enemy
+                        range_for_closest = enemy.distance
         elif self.cause == "ground_enemy_in_range":
-            pass
+            closest_enemy = None
+            range_for_closest = 999999999999999
+            for enemy in self.enemy_list:
+                if (self.x - enemy.x) ** 2 + (self.y - enemy.y) ** 2 <= self.range ** 2 and "flying" not in enemy.types:
+                    self.ready_for_action = True
+                    if enemy.distance <= range_for_closest:
+                        closest_enemy = enemy
+                        range_for_closest = enemy.distance
 
         if self.charged_time >= self.reload_time and self.ready_for_action:
             self.charged_time = 0
@@ -30,12 +62,17 @@ class Tower:
             self.action(closest_enemy)
 
     def action(self, potential_closest_enemy_or_some_other_variable_from_checker):
+        """
+        Непосредственно выполняемое башней действие, уникальное для каждой
+        :param potential_closest_enemy_or_some_other_variable_from_checker: передаёт ближайшего к финишу врага,
+        доступного для атак, или какой-либо другой параметр башни или окружающей среды, полученный в ходе работы чекера
+        """
         pass
 
 
 class ArrowTower(Tower):
-    def __init__(self, x, y):
-        super().__init__(x, y, "enemy_in_range")
+    def __init__(self, x, y, enemy_list):
+        super().__init__(x, y, "enemy_in_range", enemy_list)
         self.cost = 10
         self.range = 100
         self.reload_time = 10
@@ -46,16 +83,16 @@ class ArrowTower(Tower):
 
 
 class GunTower(Tower):
-    def __init__(self, x, y):
-        super().__init__(x, y, "enemy_in_range")
+    def __init__(self, x, y, enemy_list):
+        super().__init__(x, y, "enemy_in_range", enemy_list)
         self.cost = 15
         self.range = 75
         self.reload_time = 8
 
 
 class BombTower(Tower):
-    def __init__(self, x, y):
-        super().__init__(x, y, "ground_enemy_in_range")
+    def __init__(self, x, y, enemy_list):
+        super().__init__(x, y, "ground_enemy_in_range", enemy_list)
         self.cost = 25
         self.range = 60
         self.reload_time = 15
@@ -74,12 +111,13 @@ class Warrior:
     def __init__(self):
         """
         Инициализация класса Warrior (все живые существа, способные стоять на карте).
-        У каждого есть показатели здоровья, урона и скорости, а также прикреплённый к ним список летящих в него в
-        данный момент снарядов, перемещающихся вместе с ними
+        У каждого есть показатели здоровья, урона и скорости, соответствующая им типизация,
+        а также прикреплённый к ним список летящих в него в данный момент снарядов, перемещающихся вместе с ними
         """
         self.hp = 0
         self.dmg = 0
         self.speed = 0
+        self.types = []
         self.projectiles = []
 
 
@@ -90,6 +128,7 @@ class Opponent(Warrior):
         self.x = '-'
         self.y = '-'
         self.group = group
+        # self.distance = 0  #  FIXME self.distance достаётся из файла
 
     def move_opponent(self):
         map_types = type(self).__mro__
--- move_map_create_tool.py
+++ move_map_create_tool.py
@@ -9,18 +9,19 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 # ввод
-size = list(map(int, input('размер в клетках в формате AxB: ').split('x')))
-file_name = input('класс: ') + '_'
-file_name += input('группа: ') + '_move_map.txt'
+size = list(map(int, input("размер в клетках в формате AxB: ").split("x")))
+file_name = input("класс: ") + "_"
+file_name += input("группа: ") + "_move_map.txt"
 
 # первичная обработка данных, создание пустых массивов
 screen = pygame.display.set_mode((g_c.WIDTH, g_c.HEIGHT))
 rect_size = [g_c.WIDTH / size[0], g_c.HEIGHT / size[1]]
 map_info = []
 for i in range(size[1]):
-    map_info += [['-'] * size[0]]
+    map_info += [["-"] * size[0]]
 finished = False
 move_trajectory = []
+trajectory_length = 0
 
 # цикл создания карты перемещений
 while not finished:
@@ -45,6 +46,9 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                if len(move_trajectory) != 0:
+                    trajectory_length += ((event.pos[0] / rect_size[0] - move_trajectory[-1][0]) ** 2 + (
+                        (event.pos[1] / rect_size[1] - move_trajectory[-1][1])) ** 2) ** (1 / 2)
                 move_trajectory += [(int(event.pos[0] / rect_size[0]), int(event.pos[1] / rect_size[1]))]
             elif event.button == 3:
                 move_trajectory = move_trajectory[0:-1]
@@ -52,16 +56,15 @@ while not finished:
     clock.tick(30)
 
 image = pygame.surface.Surface((g_c.WIDTH, g_c.HEIGHT), pygame.SRCALPHA)
-color = list(map(int, input('цвет: ').split()))
-width = int(input('ширина: '))
+color = list(map(int, input("цвет: ").split()))
+width = int(input("ширина: "))
 for i in range(len(move_trajectory) - 1):
     pygame.draw.line(image, color,
                      ((move_trajectory[i][0] + 0.5) * rect_size[0],
                       (move_trajectory[i][1] + 0.5) * rect_size[1]),
                      ((move_trajectory[i + 1][0] + 0.5) * rect_size[0],
                       (move_trajectory[i + 1][1] + 0.5) * rect_size[1]), width)
-pygame.image.save(image, 'maps/new/image_' + file_name[0:-4] + '.png')
-
+pygame.image.save(image, "maps/new/image_" + file_name[0:-4] + ".png")
 
 # перенос данных из массива координат в двухмерный конечный массив
 if len(move_trajectory) >= 2:
@@ -74,12 +77,14 @@ if len(move_trajectory) >= 2:
             map_info[y][x] = math.atan(dy / dx) + math.pi / 2 * int(dx < 0)
         else:
             map_info[y][x] = math.pi / 2 * (int(dy > 0) - int(dy < 0))
-map_info[move_trajectory[-1][1]][move_trajectory[-1][0]] = 'stop'
+map_info[move_trajectory[-1][1]][move_trajectory[-1][0]] = "stop"
 map_info += [move_trajectory[0]]
 # сохранение данных
-with open('maps/new/' + file_name, 'w') as file:
+with open("maps/new/" + file_name, "w") as file:
+    file.write(str(trajectory_length) + " ")
+    file.write("\n")
     for line in map_info:
         for an in line:
-            file.write(str(an) + ' ')
-        file.write('\n')
-pygame.image.save(screen, 'maps/new/' + file_name + '_plan.png') # FIXME нужно сохранять этот файл?
+            file.write(str(an) + " ")
+        file.write("\n")
+pygame.image.save(screen, "maps/new/" + file_name + "_plan.png")  # FIXME нужно сохранять этот файл?
